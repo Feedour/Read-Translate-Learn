daa2977a56eb1056afae3192bc2e433e47c20958
meeg <golovay14@gmail.com>
2016-12-03 23:35:40 +0300

    MainActivity изменен под NavigationDrawer. Добавлена смена режима дня и ночи.

--- app/src/main/java/ru/meeg/read_translate_learn/DictionaryActivity.java
+++ app/src/main/java/ru/meeg/read_translate_learn/DictionaryActivity.java
@@ -2,20 +2,22 @@ package ru.meeg.read_translate_learn;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
 public class DictionaryActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    WordAdapter adapter;
+    private RecyclerView recyclerView;
+    private WordAdapter adapter;
+    private Toolbar toolbar;
 
     public static void start(Context context){
         Intent intent = new Intent(context, DictionaryActivity.class);
@@ -26,6 +28,11 @@ public class DictionaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         recyclerView = (RecyclerView)findViewById(R.id.dictionary_rv);
 
@@ -43,5 +50,11 @@ public class DictionaryActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/ru/meeg/read_translate_learn/MainActivity.java
+++ app/src/main/java/ru/meeg/read_translate_learn/MainActivity.java
@@ -1,23 +1,60 @@
 package ru.meeg.read_translate_learn;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.view.View;
 import android.widget.TextView;
+import android.content.res.Configuration;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener{
+
 
     private TextView textView; //используется в смене размера шрифта и добавлении слов в словарь
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer_layout);
+        Log.d("TAG","вызван метод onCreate()");
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout)findViewById(R.id.navigation_drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
         textView = (TextView)findViewById(R.id.main_textView);
         textView.setText(R.string.temp_main_text);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nvView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -26,14 +63,15 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch(item.getItemId()) {
             case R.id.main_menu_item_day_mode: {
                 Log.d("TAG_CLICK", "Пункт меню: смена режима дня/ночи");
 
+                int currentNightMode = getResources().getConfiguration().uiMode
+                        & Configuration.UI_MODE_NIGHT_MASK;
+                setNightMode(currentNightMode);
                 return true;
             }
             case R.id.main_menu_item_font: {
@@ -50,8 +88,47 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             }
+            case R.id.home:{
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setNightMode(int currentNightMode){
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:{
+                getDelegate().setLocalNightMode(
+                        AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            }
+            case Configuration.UI_MODE_NIGHT_YES:{
+                getDelegate().setLocalNightMode(
+                        AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            }
+        }
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.main_nav_menu_files) {
+            Log.d("TAG","В NavigationDrawer выбран пункт files");
+
+        } else if (id == R.id.main_nav_menu_words){
+            Log.d("TAG","В NavigationDrawer выбран пункт words");
+            DictionaryActivity.start(this);
+
+        } else if (id == R.id.main_nav_menu_settings){
+            Log.d("TAG","В NavigationDrawer выбран пункт settings");
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
